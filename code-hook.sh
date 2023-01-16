b04e8d65d88e334c95951d8a5cf066a59200b07b
Kevin Nitro <kevin.jessid@gmail.com>
2023-01-16 18:32:01 -0500

    fixing the new experiment for get new codes X6

--- code-hook.sh
+++ code-hook.sh
@@ -9,6 +9,7 @@ bd=$(tput bold)
 nobd=$(tput sgr0)
 r=$(printf '\e[31m')
 noc=$(printf '\e[0m')
+DATE=$(date +%F:%H)
 
 ver1 (){
     echo -e "\n\tVerifying if this machine currently have ${r}GIT${noc} installed...."
@@ -17,18 +18,20 @@ ver1 (){
     if [ "$verInGIT" == "$gitpath" ]; then
         echo -e "\n\t${r}GIT${noc} is already installed"
     else
+        mkdir -d $HOME/installing-GIT-log-$DATE
         echo -e "\n"
         read -s -p "Please, for go foward with installing GIT, \n insert the sudo passwd:" passwd
         echo "$passwd" | sudo -S dnf update
-        echo "$passwd" | sudo -S dnf -y install git
+        echo "$passwd" | sudo -S dnf -y install git > $HOME/installing-git-log-$DATE
     fi    
     read -n 1 -s -r -p "Press [ENTER] to continue..."
 }
 
+
 echo -e "";ver1;echo -e"\n"
-mkdir -p $HOME/code-hook-box
-git clone https://github.com/kjfigueroa/masterofuniverse.git $HOME/code-hook-box/
-chmod +x $HOME/code-hook-box/*
+mkdir -p $HOME/code-hook-box-$DATE
+git clone https://github.com/kjfigueroa/masterofuniverse.git $HOME/code-hook-box-$DATE/
+chmod +x $HOME/code-hook-box-$DATE/*
 
 cat << EOF
 
@@ -36,3 +39,7 @@ cat << EOF
 EOF
 
 exit 0
+
+
+DATE=$(date +%F:%H)
+mkdir "dir-$DATE"
