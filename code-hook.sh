9073d65c351b5132d12bd914a3f826efc0117e9f
Kevin Nitro <kevin.jessid@gmail.com>
2023-01-16 17:45:41 -0500

    fixing the new experiment for get new codes X5

--- code-hook.sh
+++ code-hook.sh
@@ -14,25 +14,25 @@ ver1 (){
     echo -e "\n\tVerifying if this machine currently have ${r}GIT${noc} installed...."
     gitpath="/usr/bin/git"
     verInGIT=$(which git)
-    if [ "verInGIT" == "gitpath" ]; then
-        #Testing
-        echo -e "SI"
-        #echo -e "\n\t${r}GIT${noc} is already installed"
+    if [ "$verInGIT" == "$gitpath" ]; then
+        echo -e "\n\t${r}GIT${noc} is already installed"
     else
-        echo -e "NO"
-        #echo -e "\n"
-        #read -s -p "Please, for go foward with installing GIT, \n insert the sudo passwd:" passwd
-        #echo "$passwd" | sudo -S dnf update
-        #echo "$passwd" | sudo -S dnf -y install git
+        echo -e "\n"
+        read -s -p "Please, for go foward with installing GIT, \n insert the sudo passwd:" passwd
+        echo "$passwd" | sudo -S dnf update
+        echo "$passwd" | sudo -S dnf -y install git
     fi    
     read -n 1 -s -r -p "Press [ENTER] to continue..."
 }
 
-echo -e "";ver1
+echo -e "";ver1;echo -e"\n"
 mkdir -p $HOME/code-hook-box
-git clone git@github.com:kjfigueroa/masterofuniverse.git $HOME/code-hook-box/
+git clone https://github.com/kjfigueroa/masterofuniverse.git $HOME/code-hook-box/
 chmod +x $HOME/code-hook-box/*
+
 cat << EOF
+
 {tab}the code was allocated in ${bd}${r}$(echo "$HOME/code-hook-box")${noc}${nobd}
 EOF
+
 exit 0
