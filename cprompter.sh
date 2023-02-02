37b0c0653447840844b276fb4b2052c6edbeef60
Kevin Jessid Figueroa Maza <68950531+kjfigueroa@users.noreply.github.com>
2023-02-02 18:28:59 -0500

    Update cprompter.sh

--- cprompter.sh
+++ cprompter.sh
@@ -3,7 +3,9 @@
 #	By Kevin J Figueroa
 # 	Just Personalizing my prompt line
 
+name=$1
+
 echo -e "\n\n#Just Personalizing my prompt line" >> ~/.bashrc
-echo "PS1='\[\033[00;97;101m\]Kevin \[\033[00;33m\]\033[00;97;100m\]\[\033[00;97;100m\][\W]\[\033[00m\]$ '" >> ~/.bashrc
+echo "PS1='\[\033[00;97;101m\]$name \[\033[00;33m\]\033[00;97;100m\]\[\033[00;97;100m\][\W]\[\033[00m\]$ '" >> ~/.bashrc
 source ~/.bashrc
 exit 0
